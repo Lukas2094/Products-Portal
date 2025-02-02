f5ae2e49830c793cbb1044fb7a4c395b03d0406f
Lukas2094 <lukas2_8@hotmail.com>
2025-02-02 20:46:04 -0300

    optmization build props

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,14 +6,14 @@ import Header from "../../../components/Header/Header";
 import { Box, Container, Typography } from "@mui/material";
 import { fetchProducts } from "../api/productsApi/getProducts";
 
-export default async function ProductsPage({ searchParams }: { searchParams: { q: string } }) {
+export default async function ProductsPage({ searchParams }: { searchParams: Promise<{ q: string }> }) {
     const params = await searchParams;
     const query = params.q ?? "";
     const products = await fetchProducts(query);
     
     return (
         <>
-            <Header src="/images/gif/carSale-unscreen.gif" params={searchParams.q} />
+            <Header src="/images/gif/carSale-unscreen.gif" params={query} />
             <Container className="p-5">
                 {products.length === 0 ? (
                     <Box className="flex items-center justify-center mt-28">
