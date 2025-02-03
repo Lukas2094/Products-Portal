b4524314a1d2d0ac427cc40ce1fb59f0dee6181f
Lukas2094 <lukas2_8@hotmail.com>
2025-02-02 21:35:42 -0300

    remove comments

--- src/app/api/auth/[...nextauth]/[...nextauth].ts
+++ src/app/api/auth/[...nextauth]/[...nextauth].ts
@@ -1,37 +1,6 @@
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-
-// export const authOptions = {
-//     providers: [
-//         CredentialsProvider({
-//             name: "Credentials",
-//             credentials: {
-//                 username: { label: "Username", type: "text" },
-//                 password: { label: "Password", type: "password" },
-//             },
-//             async authorize(credentials) {
-//                 if (
-//                     credentials?.username === "Codako" &&
-//                     credentials?.password === "Teste123@"
-//                 ) {
-//                     return { id: "1", name: "Codako" };
-//                 }
-//                 return null;
-//             },
-//         }),
-//     ],
-//     pages: {
-//         signIn: "/login",
-//     },
-// };
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { JWT } from "next-auth/jwt"; // Importando a tipagem JWT
+import { JWT } from "next-auth/jwt"; 
 
 export const authOptions = {
   providers: [
@@ -56,7 +25,6 @@ export const authOptions = {
     signIn: "/login",
   },
   callbacks: {
-    // Tipando a variável `token` com JWT
     async jwt({ token, account }: { token: JWT, account: any }) {
       if (account) {
         token.accessToken = account.access_token;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,6 @@
-// import { NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
-
-// export default async function middleware(req : any) {
-//     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    
-//     if (!token && req.nextUrl.pathname.startsWith("/products")) {
-//         return NextResponse.redirect(new URL("/login", req.url));
-//     }
-    
-//     if (token && req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/") {
-//         return NextResponse.redirect(new URL("/products", req.url));
-//     }
-
-//     return NextResponse.next(); 
-// }
-
-// export const config = {
-//     matcher: ["/products/:path*", "/login"], 
-// };
-
-
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { NextRequest } from "next/server"; // Importando o tipo NextRequest
+import { NextRequest } from "next/server"; 
 
 export default async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
